f5a3e9eb3b476ea490d209a58d18d8fef34bf040
Abhinandan <jhaabhinandan55@gmail.com>
2024-05-18 20:11:53 +0530

    initial setup done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home';
+import About from './pages/About';
+import Signin from './pages/Signin';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
+import Header from './components/Header';
 
 const App = () => {
   return (
-    <div className='text-red-500'>
-      <h1>Hello</h1>
-    </div>
+    <BrowserRouter>
+    <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/signin' element={<Signin/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaSearch } from 'react-icons/fa';
+
+const Header = () => {
+  return (
+    <header className="bg-slate-200 shadow-md">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <Link to="/">
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+            <span className="text-slate-500">Rentify -</span>
+            <span className="text-slate-700">- Presidio</span>
+          </h1>
+        </Link>
+        <form
+          // onSubmit={handleSubmit}
+          className="bg-slate-100 p-3 rounded-lg flex items-center"
+        >
+          <input
+            type="text"
+            placeholder="Search..."
+            className="bg-transparent focus:outline-none w-24 sm:w-64"
+            // value={searchTerm}
+            // onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <button>
+            <FaSearch className="text-slate-600" />
+          </button>
+        </form>
+        <ul className="flex gap-4">
+          <Link to="/">
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              Home
+            </li>
+          </Link>
+          <Link to="/about">
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              About
+            </li>
+          </Link>
+
+          <Link to="/profile">
+            {/* {currentUser ? (
+              <img
+                src={currentUser.avatar}
+                alt="avatar"
+                className="rounded-full h-7 w-7 object-cover"
+              />
+            ) : (
+              <li className="text-slate-700 hover:underline">Sign In</li>
+            )} */}
+          </Link>
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
